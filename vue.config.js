ae1b7f7d3942d3f345db1036e3b138d5d08294d7
Gian Pinedo <gppproyect@gmail.com>
2023-11-30 18:39:33 -0500

    first deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/gianpinedo/culquitest/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/GianPinedo/culquitest/' : '/',
   transpileDependencies: ['vue'],
 }
