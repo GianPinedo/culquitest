bb805b0832797062fad6b3c99c955262f7aa5f38
Joshua <josh@Joshuas-MacBook-Air.local>
2023-12-01 00:28:00 -0500

    “mm“

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/GianPinedo/culquitest/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/culquitest/' : '/',
   transpileDependencies: ['vue'],
 }
